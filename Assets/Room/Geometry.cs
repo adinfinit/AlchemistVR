a83596c6017a1e35ad4104084e1831f59910b973
Egon Elbre <egonelbre@gmail.com>
2017-08-05 02:10:33 +0300

    Add cube primitive.

--- Assets/Room/Geometry.cs
+++ Assets/Room/Geometry.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public static class Geometry
 {
-	public static GameObject CylinderBetweenPoints (Vector3 p1, Vector3 p2, float radius)
+	public static GameObject PrimitiveBetweenPoints (PrimitiveType primitive, Vector3 p1, Vector3 p2, float radius)
 	{
 		Vector3 offset = p2 - p1;
 		Vector3 pos = p1 + (offset / 2);
 
-		GameObject cylinder = GameObject.CreatePrimitive (PrimitiveType.Cylinder);
-		cylinder.transform.position = pos;
+		GameObject obj = GameObject.CreatePrimitive (primitive);
+		obj.transform.position = pos;
 
 		Vector3 scale = new Vector3 (radius, offset.magnitude / 2.0f, radius);
-		cylinder.transform.up = offset;
-		cylinder.transform.localScale = scale;
+		obj.transform.up = offset;
+		obj.transform.localScale = scale;
 
-		return cylinder;
+		return obj;
+	}
+
+	public static GameObject CylinderBetweenPoints (Vector3 p1, Vector3 p2, float radius)
+	{
+		return PrimitiveBetweenPoints (PrimitiveType.Cylinder, p1, p2, radius);
 	}
 
 	public static GameObject CapsuleBetweenPoints (Vector3 p1, Vector3 p2, float radius)
 	{
-		Vector3 offset = p2 - p1;
-		Vector3 pos = p1 + (offset / 2);
+		return PrimitiveBetweenPoints (PrimitiveType.Capsule, p1, p2, radius);
+	}
 
-		GameObject cylinder = GameObject.CreatePrimitive (PrimitiveType.Capsule);
-		cylinder.transform.position = pos;
-
-		Vector3 scale = new Vector3 (radius, offset.magnitude / 2.0f, radius);
-		cylinder.transform.up = offset;
-		cylinder.transform.localScale = scale;
-
-		return cylinder;
+	public static GameObject CubeBetweenPoints (Vector3 p1, Vector3 p2, float radius)
+	{
+		return PrimitiveBetweenPoints (PrimitiveType.Cube, p1, p2, radius);
 	}
 
 	public static GameObject Sphere (Vector3 p, float radius)
